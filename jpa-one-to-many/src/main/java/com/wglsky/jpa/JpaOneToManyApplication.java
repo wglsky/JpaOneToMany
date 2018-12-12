9570e5357a151586b6dce499e403d01c0f99ccf3
wglsky <40151406+wglsky@users.noreply.github.com>
2018-12-12 16:49:25 +0100

    Created project

---
+++ jpa-one-to-many/src/main/java/com/wglsky/jpa/JpaOneToManyApplication.java
@@ -0,0 +1,14 @@
+package com.wglsky.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaOneToManyApplication.class, args);
+	}
+
+}
+
